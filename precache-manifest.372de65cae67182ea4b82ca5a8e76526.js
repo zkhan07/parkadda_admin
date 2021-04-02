448431471463cc98d8a5ce8a4f81c3101dda76f4
khan zaid <khanzaid786786.kz@gmail.com>
2021-04-03 00:05:05 +0530

    Updates

---
+++ precache-manifest.372de65cae67182ea4b82ca5a8e76526.js
@@ -0,0 +1,330 @@
+self.__precacheManifest = [
+  {
+    "revision": "562bee4936ca2b32d322",
+    "url": "/parkadda_admin/static/js/0.92363c15.chunk.js"
+  },
+  {
+    "revision": "56911ad7c6e72547b779",
+    "url": "/parkadda_admin/static/js/1.a128a300.chunk.js"
+  },
+  {
+    "revision": "a673a846700337f1bd66",
+    "url": "/parkadda_admin/static/css/2.c9a56cf2.chunk.css"
+  },
+  {
+    "revision": "a673a846700337f1bd66",
+    "url": "/parkadda_admin/static/js/2.49856585.chunk.js"
+  },
+  {
+    "revision": "859038e29888e79bcafd",
+    "url": "/parkadda_admin/static/js/3.204ce3f5.chunk.js"
+  },
+  {
+    "revision": "6ed0c7c5178e0d8a39f9",
+    "url": "/parkadda_admin/static/js/4.4a8b9d90.chunk.js"
+  },
+  {
+    "revision": "ea71f44895d6326543b1",
+    "url": "/parkadda_admin/static/js/5.bb0f446c.chunk.js"
+  },
+  {
+    "revision": "c4434f72d188a4eff566",
+    "url": "/parkadda_admin/static/css/main.eb80c067.chunk.css"
+  },
+  {
+    "revision": "c4434f72d188a4eff566",
+    "url": "/parkadda_admin/static/js/main.42f9021f.chunk.js"
+  },
+  {
+    "revision": "2f500d5d1945f3c0f151",
+    "url": "/parkadda_admin/static/js/runtime~main.278b65ad.js"
+  },
+  {
+    "revision": "0044c9d212c79c880070",
+    "url": "/parkadda_admin/static/css/8.755704de.chunk.css"
+  },
+  {
+    "revision": "0044c9d212c79c880070",
+    "url": "/parkadda_admin/static/js/8.b38e5345.chunk.js"
+  },
+  {
+    "revision": "61504da4a14404495b0e",
+    "url": "/parkadda_admin/static/css/9.755704de.chunk.css"
+  },
+  {
+    "revision": "61504da4a14404495b0e",
+    "url": "/parkadda_admin/static/js/9.116eff58.chunk.js"
+  },
+  {
+    "revision": "b5b2185ac71e9c530409",
+    "url": "/parkadda_admin/static/css/10.755704de.chunk.css"
+  },
+  {
+    "revision": "b5b2185ac71e9c530409",
+    "url": "/parkadda_admin/static/js/10.b191efe9.chunk.js"
+  },
+  {
+    "revision": "22d0eeca3a58c124d868",
+    "url": "/parkadda_admin/static/js/11.2b33e2e0.chunk.js"
+  },
+  {
+    "revision": "df0df4000633a2c835b4",
+    "url": "/parkadda_admin/static/js/12.5d0410f8.chunk.js"
+  },
+  {
+    "revision": "866770832c2a395be0db",
+    "url": "/parkadda_admin/static/css/13.755704de.chunk.css"
+  },
+  {
+    "revision": "866770832c2a395be0db",
+    "url": "/parkadda_admin/static/js/13.025026e5.chunk.js"
+  },
+  {
+    "revision": "04c1fbf695d66b12facc",
+    "url": "/parkadda_admin/static/css/14.755704de.chunk.css"
+  },
+  {
+    "revision": "04c1fbf695d66b12facc",
+    "url": "/parkadda_admin/static/js/14.9dfe07fd.chunk.js"
+  },
+  {
+    "revision": "431078487d9a105f2e8e",
+    "url": "/parkadda_admin/static/css/15.755704de.chunk.css"
+  },
+  {
+    "revision": "431078487d9a105f2e8e",
+    "url": "/parkadda_admin/static/js/15.c55f5932.chunk.js"
+  },
+  {
+    "revision": "5fd78d55c0e470f878be",
+    "url": "/parkadda_admin/static/css/16.755704de.chunk.css"
+  },
+  {
+    "revision": "5fd78d55c0e470f878be",
+    "url": "/parkadda_admin/static/js/16.2c2b4188.chunk.js"
+  },
+  {
+    "revision": "3cbf057ec03ffb95bec9",
+    "url": "/parkadda_admin/static/css/17.755704de.chunk.css"
+  },
+  {
+    "revision": "3cbf057ec03ffb95bec9",
+    "url": "/parkadda_admin/static/js/17.4e687f7c.chunk.js"
+  },
+  {
+    "revision": "c8a1b41c38f77279b2f4",
+    "url": "/parkadda_admin/static/css/18.755704de.chunk.css"
+  },
+  {
+    "revision": "c8a1b41c38f77279b2f4",
+    "url": "/parkadda_admin/static/js/18.833ba38d.chunk.js"
+  },
+  {
+    "revision": "03abd5c523d2970436a1",
+    "url": "/parkadda_admin/static/css/19.755704de.chunk.css"
+  },
+  {
+    "revision": "03abd5c523d2970436a1",
+    "url": "/parkadda_admin/static/js/19.95c5522e.chunk.js"
+  },
+  {
+    "revision": "4af1df9620c66996d637",
+    "url": "/parkadda_admin/static/css/20.755704de.chunk.css"
+  },
+  {
+    "revision": "4af1df9620c66996d637",
+    "url": "/parkadda_admin/static/js/20.4e38064e.chunk.js"
+  },
+  {
+    "revision": "b4e7909e4de0cf8cb8fb",
+    "url": "/parkadda_admin/static/js/21.a2ba0590.chunk.js"
+  },
+  {
+    "revision": "9f7350f2a230b4afedf7",
+    "url": "/parkadda_admin/static/js/22.9f198abe.chunk.js"
+  },
+  {
+    "revision": "0e05c1517bb7ecd22ee1",
+    "url": "/parkadda_admin/static/js/23.0e66c4be.chunk.js"
+  },
+  {
+    "revision": "892c267919d38be05fa5",
+    "url": "/parkadda_admin/static/js/24.c9ffb32b.chunk.js"
+  },
+  {
+    "revision": "f0cbb8688f0f65585e5c",
+    "url": "/parkadda_admin/static/js/25.76083672.chunk.js"
+  },
+  {
+    "revision": "ed62aa6a7a69979dcb51b0617170f544",
+    "url": "/parkadda_admin/static/media/blueMenuBarIcon.ed62aa6a.svg"
+  },
+  {
+    "revision": "b43dc17b38170d84999945e29044409e",
+    "url": "/parkadda_admin/static/media/whiteLogo.b43dc17b.jpg"
+  },
+  {
+    "revision": "c485935db8a4a6ae099982fb334d524c",
+    "url": "/parkadda_admin/static/media/homeIcon.c485935d.svg"
+  },
+  {
+    "revision": "f650ba34d9804ed6183ed6f79a2f6c3b",
+    "url": "/parkadda_admin/static/media/userIcon.f650ba34.svg"
+  },
+  {
+    "revision": "cc7c1ca7625814e88df7d5df2c9b39f5",
+    "url": "/parkadda_admin/static/media/parkingIcon.cc7c1ca7.svg"
+  },
+  {
+    "revision": "944f5ddaf59186b49663c027fcc7dc01",
+    "url": "/parkadda_admin/static/media/monthlyPassIcon.944f5dda.svg"
+  },
+  {
+    "revision": "822cf41384e7b4598f25b5e49ff234e5",
+    "url": "/parkadda_admin/static/media/bookingIcon.822cf413.svg"
+  },
+  {
+    "revision": "bd88848194cf55531e6946e0c3d5daab",
+    "url": "/parkadda_admin/static/media/hardwareIcon.bd888481.svg"
+  },
+  {
+    "revision": "37a009190207a921ea21a31475546adc",
+    "url": "/parkadda_admin/static/media/supportIcon.37a00919.svg"
+  },
+  {
+    "revision": "fdd71a776296d2557fa27c5944446d23",
+    "url": "/parkadda_admin/static/media/paymentIcon.fdd71a77.svg"
+  },
+  {
+    "revision": "54a7a6db8bd501b7866bf1b196921a78",
+    "url": "/parkadda_admin/static/media/notificationIcon.54a7a6db.svg"
+  },
+  {
+    "revision": "2c5a551d888952d87a26eed67622aa16",
+    "url": "/parkadda_admin/static/media/blue-emailIcon.2c5a551d.svg"
+  },
+  {
+    "revision": "b73142dcf0a438b43ed61b8f52e6cdec",
+    "url": "/parkadda_admin/static/media/blueBellIcon.b73142dc.svg"
+  },
+  {
+    "revision": "4e63b6de46b1faf06b9a8b7a2404d17f",
+    "url": "/parkadda_admin/static/media/question-markIcon.4e63b6de.svg"
+  },
+  {
+    "revision": "42bcebb3a44c7aea3e32d18eb3221676",
+    "url": "/parkadda_admin/static/media/whiteBookingIcon.42bcebb3.svg"
+  },
+  {
+    "revision": "18f6c52c0fdba89c5512dead30658953",
+    "url": "/parkadda_admin/static/media/parkMumbai.18f6c52c.png"
+  },
+  {
+    "revision": "09cce601313def0f682004938899692b",
+    "url": "/parkadda_admin/static/media/indianRupeeIcon.09cce601.svg"
+  },
+  {
+    "revision": "a73a8535d1bf2dd7ec090d08c5efa0c4",
+    "url": "/parkadda_admin/static/media/growupImg.a73a8535.svg"
+  },
+  {
+    "revision": "1a77b86296735acb63c530cc7a897be0",
+    "url": "/parkadda_admin/static/media/blueBikeIcon.1a77b862.svg"
+  },
+  {
+    "revision": "edcee728f2d134633eefef9ec5818af3",
+    "url": "/parkadda_admin/static/media/blueAutoIcon.edcee728.svg"
+  },
+  {
+    "revision": "9590f6e10be09c02447370c8066bc46e",
+    "url": "/parkadda_admin/static/media/blueCarIcon.9590f6e1.svg"
+  },
+  {
+    "revision": "3c7ef350df3e9deab7c78bf04533d07e",
+    "url": "/parkadda_admin/static/media/blueJeepIcon.3c7ef350.svg"
+  },
+  {
+    "revision": "3b58d648d2256150baa150675378dafc",
+    "url": "/parkadda_admin/static/media/blueTruckIcon.3b58d648.svg"
+  },
+  {
+    "revision": "b0ad1fbd45074a0e029107a63f619f0c",
+    "url": "/parkadda_admin/static/media/blueECarIcon.b0ad1fbd.svg"
+  },
+  {
+    "revision": "d26f5d7b80872247c35b1a26311d8f05",
+    "url": "/parkadda_admin/static/media/emailIcon.d26f5d7b.svg"
+  },
+  {
+    "revision": "32718227c07b496e7efc81100ce98f68",
+    "url": "/parkadda_admin/static/media/whiteBellIcon.32718227.svg"
+  },
+  {
+    "revision": "6f82bd9fa1467d93e440676e0d4c4dd3",
+    "url": "/parkadda_admin/static/media/whiteMenuBarIcon.6f82bd9f.svg"
+  },
+  {
+    "revision": "0b63cd07b53019d548e3e95000a179c0",
+    "url": "/parkadda_admin/static/media/hardwareBoomBarrierImg.0b63cd07.svg"
+  },
+  {
+    "revision": "047d9dd6aa8698d01f1d1c534a444b87",
+    "url": "/parkadda_admin/static/media/hardwareRFIDImg.047d9dd6.svg"
+  },
+  {
+    "revision": "c5f331ca75eb5b7ca682fba7d3e97e7f",
+    "url": "/parkadda_admin/static/media/hardwareServerImg.c5f331ca.svg"
+  },
+  {
+    "revision": "d12b5abfdced25c7e9231b7856741f9a",
+    "url": "/parkadda_admin/static/media/hardwarePOSMachineImg.d12b5abf.svg"
+  },
+  {
+    "revision": "b5d38e5b9d0ad7512df5be8d1d160471",
+    "url": "/parkadda_admin/static/media/hardwareScannerImg.b5d38e5b.svg"
+  },
+  {
+    "revision": "7d3a662412e504659a6e1769ce603123",
+    "url": "/parkadda_admin/static/media/hardwareFareImg.7d3a6624.svg"
+  },
+  {
+    "revision": "57ad13b2175829b99a2bcb6c548c77d2",
+    "url": "/parkadda_admin/static/media/hardwareCashBoxImg.57ad13b2.svg"
+  },
+  {
+    "revision": "43b4b2ac8bbddf164ed18ad53defd7fa",
+    "url": "/parkadda_admin/static/media/hardwareLoopDetectorImg.43b4b2ac.svg"
+  },
+  {
+    "revision": "16c08b7acb70f508eeb12524ab8b48eb",
+    "url": "/parkadda_admin/static/media/hardwareTicketMachineImg.16c08b7a.svg"
+  },
+  {
+    "revision": "fd5324fa66603109b5f990f6a2e6c019",
+    "url": "/parkadda_admin/static/media/hardwareControllerImg.fd5324fa.svg"
+  },
+  {
+    "revision": "caa2b9759a38dc7278575ef2c0a861de",
+    "url": "/parkadda_admin/static/media/hardwarePrinterIcon.caa2b975.svg"
+  },
+  {
+    "revision": "5a03400cdb706725a0ab36a1864146dd",
+    "url": "/parkadda_admin/static/media/hardwareRFIDIcon.5a03400c.svg"
+  },
+  {
+    "revision": "b16784f98866da8a9d3ca35c046bd221",
+    "url": "/parkadda_admin/static/media/hardwareTicketDispenserIcon.b16784f9.svg"
+  },
+  {
+    "revision": "11fdbef864345967d8ddfe42e7e1fb52",
+    "url": "/parkadda_admin/static/media/hardwareANPRIcon.11fdbef8.svg"
+  },
+  {
+    "revision": "3f0debbf9f38c883b737d6752c2d6d0e",
+    "url": "/parkadda_admin/static/media/hardwarePageImg.3f0debbf.svg"
+  },
+  {
+    "revision": "eb3cc3e757bac796d3e8801989d6c6de",
+    "url": "/parkadda_admin/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/parkadda_admin/precache-manifest.372de65cae67182ea4b82ca5a8e76526.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/parkadda_admin/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
